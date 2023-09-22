4cd79c9bd3c1445e51d7e431ee73d7db1bb93aa8
Jaxbel <jazziel.bello193@tectijuana.edu.mx>
2023-09-22 01:58:31 -0700

    cambios funcionales

--- rickapi.js
+++ rickapi.js
@@ -1,71 +1,69 @@
+// Función para buscar la location y mostrar los primeros 5 residents
+function buscarValoresPorID() {
+    
+    const locationId = parseInt(document.getElementById('input').value); 
+    const resultElement = document.getElementById('result');
+    const residentsElement = document.getElementById('residents');
 
-function getCharacters(character){
 
-    const results = fetch("https://rickandmortyapi.com/api/character");
-    results
+    const locationURL = `https://rickandmortyapi.com/api/location/${locationId}`;
+    
+  
+    fetch(locationURL)
         .then(response => response.json())
         .then(data => {
-            character(data)
+            if (data.error) {2
+                resultElement.textContent = 'Localización no encontrada';
+            } else {
+                
+                resultElement.textContent = `Nombre de la localización: ${data.name}`;
+
+                // Obtener los primeros 5 residents
+                const firstFiveResidents = data.residents.slice(0, 5);
+                
+
+                // Imprimir los primeros 5 residents
+                
+                firstFiveResidents.forEach(residentURL => {
+                    fetch(residentURL)
+                        .then(response => response.json())
+                        .then(residentData => {
+                            console.log(residentData.origin)
+                            residentsElement.innerHTML += `
+
+                            <article>
+                                <div class="card">
+                                    <img src="${residentData.image}" class="card-img-top" >
+                                    <div class="card-body">
+                                        <h5 class="name">Name: ${residentData.name}</h5>
+                                        <p class="status">Status: ${residentData.status}</p>
+                                        <p class="species">Species: ${residentData.species}</p>
+                                        <p class="origin">Origin: ${residentData.origin.name}</p>
+                                        <p>Episodes</p>
+                                        <ul>
+                                            <a class="link-epi" href="${residentData.episode}">${residentData.episode.slice(0, 3).join(', ')}</a>
+                                        </ul>
+                                    </div>
+                                </div>  
+                            </article>
+                            
+                            `;
+                            
+                        })
+                        .catch(error => {
+                            console.error('Error al obtener datos del resident:', error);
+                        });
+                        residentsElement.innerHTML = '';
+                });
+                
+            }
+        })
+        .catch(error => {
+            console.error('Error al consultar la API:', error);
+            resultElement.textContent = 'Error al consultar la API';
         });
-
-}
-getCharacters(data => {
-    data.results.forEach(personaje => {
-        const article = document.createRange().createContextualFragment(`
-            <article>
-                <div class="card" style="width: 18rem;">
-                    <img src="${personaje.image}" class="card-img-top" alt="personaje">
-                    <div class="card-body">
-                        <h5 class="name">${personaje.name}</h5>
-                        <p class="status">${personaje.status}</p>
-                        <p class="species">${personaje.species}</p>
-                        <p class="origin">${personaje.origin.name}</p>
-                        <p>Episodes</p>
-                        <ul><a class="link-epi">${personaje.episode.slice(0, 3).join(', ')}</a></ul>
-                    </div>
-                </div>  
-            </article>
-        `);
-
-        const main = document.querySelector("main");
-        main.appendChild(article);
-    });
-});
-
-
-
-
-function getLocation(location){
-
-    const results = fetch("https://rickandmortyapi.com/api/location");
-    results
-        .then(response => response.json())
-        .then(data => {
-            location(data)
-        });
-
+        
 }
 
+document.getElementById('search-btn').addEventListener('click', buscarValoresPorID);
 
-
-axios.get(`https://rickandmortyapi.com/api/location/${planetNumber}`)
-.then(response => {
-    const data = response.data;
-
-    if (data.error) {
-        alert("Planeta no encontrado");
-    } else {
-        // Obtener la lista de residentes del planeta
-        const residents = data.residents;
-
-        // Mostrar los primeros 5 personajes
-        const resultsContainer = document.getElementById("results");
-        resultsContainer.innerHTML = "<h2>Primeros 5 Personajes:</h2>";
-        for (let i = 0; i < Math.min(5, residents.length); i++) {
-            resultsContainer.innerHTML += `<p>${residents[i]}</p>`;
-        }
-    }
-})
-.catch(error => {
-    console.error("Error al realizar la solicitud a la API:", error);
-});
